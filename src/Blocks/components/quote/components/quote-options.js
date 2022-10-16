9edf34e25e9cca9c69e8c4988ae68066a4abc2ef
Sime Glavan <shgshin@gmail.com>
2022-10-16 19:14:41 +0200

    addition of quote block and component

---
+++ src/Blocks/components/quote/components/quote-options.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { __ } from '@wordpress/i18n';
+import { props, getOptions, checkAttr, getAttrKey, ComponentUseToggle } from '@eightshift/frontend-libs/scripts';
+import { ParagraphOptions } from '../../paragraph/components/paragraph-options';
+import manifest from './../manifest.json';
+
+export const QuoteOptions = (attributes) => {
+	const {
+		title: manifestTitle,
+	} = manifest;
+
+	const {
+		setAttributes,
+		label = manifestTitle,
+		quoteShowControls = true,
+
+		showQuoteUse = false,
+		showLabel = false,
+	} = attributes;
+
+	if (!quoteShowControls) {
+		return null;
+	}
+
+	const quoteUse = checkAttr('quoteUse', attributes, manifest);
+
+	return (
+		<>
+			<ComponentUseToggle
+				label={label}
+				checked={quoteUse}
+				onChange={(value) => setAttributes({ [getAttrKey('quoteUse', attributes, manifest)]: value })}
+				showUseToggle={showQuoteUse}
+				showLabel={showLabel}
+			/>
+
+			{quoteUse &&
+				<>
+					<ParagraphOptions
+						label={__('Quote', 'infinum')}
+						showLabel={true}
+						{...props('quote', attributes, {
+							options: getOptions(attributes, manifest),
+						})}
+					/>
+
+					<ParagraphOptions
+						label={__('Attribution', 'infinum')}
+						showLabel={true}
+						showParagraphUse={true}
+						{...props('attribution ', attributes, {
+							options: getOptions(attributes, manifest),
+						})}
+					/>
+				</>
+			}
+		</>
+	);
+};
---
+++ src/Blocks/components/quote/quote.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Template for the Quote Component.
+ *
+ * @package Infinum
+ */
+
+use InfinumVendor\EightshiftLibs\Helpers\Components;
+
+$globalManifest = Components::getManifest(dirname(__DIR__, 2));
+$manifest = Components::getManifest(__DIR__);
+
+$quoteUse = Components::checkAttr('quoteUse', $attributes, $manifest);
+if (!$quoteUse) {
+	return;
+}
+
+$componentClass = $manifest['componentClass'] ?? '';
+$additionalClass = $attributes['additionalClass'] ?? '';
+$blockClass = $attributes['blockClass'] ?? '';
+$selectorClass = $attributes['selectorClass'] ?? $componentClass;
+
+$quoteClass = Components::classnames([
+	Components::selector($componentClass, $componentClass),
+	Components::selector($blockClass, $blockClass, $selectorClass),
+	Components::selector($additionalClass, $additionalClass),
+]);
+
+?>
+
+<figure class="<?php echo esc_attr($quoteClass); ?>">
+	<i class="<?php echo esc_attr("{$componentClass}__icon"); ?>">
+		<?php echo $manifest['resources']['icon']; // phpcs:ignore Eightshift.Security.ComponentsEscape.OutputNotEscaped ?>
+	</i>
+
+	<blockquote class="<?php echo esc_attr("{$componentClass}__content"); ?>">
+		<?php
+		echo Components::render(
+			'paragraph',
+			Components::props('quote', $attributes, [
+				'blockClass' => $componentClass
+			])
+		);
+		?>
+	</blockquote>
+
+	<div class="<?php echo esc_attr("{$componentClass}__separator"); ?>"></div>
+
+	<figcaption class="<?php echo esc_attr("{$componentClass}__caption"); ?>">
+		<?php
+		echo Components::render(
+			'paragraph',
+			Components::props('attribution', $attributes, [
+				'blockClass' => $componentClass
+			])
+		);
+		?>
+	</figcaption>
+</figure>
